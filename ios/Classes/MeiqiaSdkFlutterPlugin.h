ea3e29c0952a0673062cbef8cca6716e8f106dc7
xukeqiang <312965601@qq.com>
2022-05-13 10:34:17 +0800

    first init

---
+++ ios/Classes/MeiqiaSdkFlutterPlugin.h
@@ -0,0 +1,4 @@
+#import <Flutter/Flutter.h>
+
+@interface MeiqiaSdkFlutterPlugin : NSObject<FlutterPlugin>
+@end
---
+++ ios/Classes/MeiqiaSdkFlutterPlugin.m
@@ -0,0 +1,20 @@
+#import "MeiqiaSdkFlutterPlugin.h"
+
+@implementation MeiqiaSdkFlutterPlugin
++ (void)registerWithRegistrar:(NSObject<FlutterPluginRegistrar>*)registrar {
+  FlutterMethodChannel* channel = [FlutterMethodChannel
+      methodChannelWithName:@"meiqia_sdk_flutter"
+            binaryMessenger:[registrar messenger]];
+  MeiqiaSdkFlutterPlugin* instance = [[MeiqiaSdkFlutterPlugin alloc] init];
+  [registrar addMethodCallDelegate:instance channel:channel];
+}
+
+- (void)handleMethodCall:(FlutterMethodCall*)call result:(FlutterResult)result {
+  if ([@"getPlatformVersion" isEqualToString:call.method]) {
+    result([@"iOS " stringByAppendingString:[[UIDevice currentDevice] systemVersion]]);
+  } else {
+    result(FlutterMethodNotImplemented);
+  }
+}
+
+@end
